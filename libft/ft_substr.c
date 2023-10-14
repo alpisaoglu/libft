84aba1c3bb30789b244f8f8044c169308ff0794b
Alperen Isaoglu <aisaoglu@k2m12s02.42kocaeli.com.tr>
2023-10-14 12:49:29 +0300

    Test2

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -5,38 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aisaoglu <aisaoglu@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/14 01:56:10 by aisaoglu          #+#    #+#             */
-/*   Updated: 2023/10/14 10:19:29 by aisaoglu         ###   ########.fr       */
+/*   Created: 2023/10/14 10:14:33 by fucalisk          #+#    #+#             */
+/*   Updated: 2023/10/14 12:47:34 by aisaoglu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-char *ft_substr(char const *s, unsigned int start, size_t len)
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	char	*substr;
 	size_t	i;
 
+	substr = (char *)malloc(len + 1);
 	i = 0;
-	while (*s != '\0')
+	if (!substr || !s)
+		return (NULL);
+	while (s[i + start] && i < len && start < ft_strlen(s))
 	{
+		substr[i] = s[i + start];
 		i++;
 	}
-	if(i >= len)
-		return (NULL);
-	char *new_str = (char *)malloc(len+1);
-	while(len > i)
-	{
-		if(s[start +i] != '\0')
-			return (NULL);
-		new_str[i] = s[start +i];
-	}
-}
-
-
-int main()
-{
-	char *t = "talhayaylayımben";
-	unsigned int i = 5;
-	size_t si = 5;
-	printf("%s",ft_substr(t,i,si)); 
+	substr[i] = '\0';
+	return (substr);
 }
